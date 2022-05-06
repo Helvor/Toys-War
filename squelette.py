01f0f79223a51fb43cf1e8aa9ac499cb7aa6f03f
Helvor <legros.victor@hotmail.com>
2022-05-06 16:38:48 +0200

    06-05-22 || Improve the menu

--- squelette.py
+++ squelette.py
@@ -4,20 +4,28 @@ from Game import *
 
 
 if __name__ == "__main__":
-    print("""
- _____                   __    __           
-/__   \___  _   _ ___   / / /\ \ \__ _ _ __ 
-  / /\/ _ \| | | / __|  \ \/  \/ / _` | '__|
- / / | (_) | |_| \__ \   \  /\  / (_| | |   
- \/   \___/ \__, |___/    \/  \/ \__,_|_|   
-            |___/                           """)
-    player1 = Player(input("Nom joueur 1 : "), 20, 10)
-    player2 = Player(input("Nom joueur 2 : "), 20, 10)
-    game = Game(player1, player2)
-    game.play()
-    if player1.life <= 0:
-        print(f"Bravo {player2.name}")
-    elif player2.life <= 0:
-        print(f"Bravo {player1.name}")
+    exit = ""
+    while not exit == "M" or not exit == "m":
+        print("""
+     _____                   __    __           
+    /__   \___  _   _ ___   / / /\ \ \__ _ _ __ 
+      / /\/ _ \| | | / __|  \ \/  \/ / _` | '__|
+     / / | (_) | |_| \__ \   \  /\  / (_| | |   
+     \/   \___/ \__, |___/    \/  \/ \__,_|_|   
+                |___/                           """)
+        print("Welcome in -TOYS WAR- || a game coded in python\n"
+              "Available game mode :\n"
+              "1 - Player vs Player\n"
+              "2 - Player vs IA")
+        choice_game = int(input("Game mode choice : "))
+        if choice_game == 1:
+            player1 = Player(input("Player 1 : "), 20, 10)
+            player2 = Player(input("Player 2 : "), 20, 10)
+            game = Game(player1, player2)
+            game.play()
+            if player1.life <= 0:
+                print(f"Great Job {player2.name} ! You won the game")
+            elif player2.life <= 0:
+                print(f"Great Job {player1.name} ! You won the game")
 
-    exit = input("Veuillez quitter la fenetre")
+            exit = input("Enter (M) for returning to menu : ")
