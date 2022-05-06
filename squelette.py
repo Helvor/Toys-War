c548b89c2e6c446f6131fe6a5944e605ada89cb6
Helvor <legros.victor@hotmail.com>
2022-05-06 15:32:06 +0200

    06-05-22 || Separate file

--- squelette.py
+++ squelette.py
@@ -1,4 +1,3 @@
-from Player import *
 from Character import *
 
 class Game:
@@ -106,7 +105,98 @@ class Game:
             self.play_turn()
             self.player_turn = 1 - self.player_turn
 
+class Player:
 
+    def __str__(self):
+        return f"Nom : {self.name} | Nombre de vie : {self.life} | Argent : {self.money} | Team : {self.team}"
+
+    def __init__(self, name, life, money):
+        """
+        PARAM : - name : str
+                - life : float
+                - money : float
+        initialisate team to empty list, game and direction to None
+        """
+        self.name = name
+        self.life = life
+        self.money = money
+        self.team = []
+        self.game = None
+        self.direction = None
+
+    @property
+    def is_alive(self):
+        return self.life > 0
+
+    def get_hit(self, damages):
+        """
+        Take the damage to life
+        PARAM : - damages : float
+        """
+        self.life -= damages
+
+    def new_character(self):
+        """
+        Ask to player where add a new Character,
+        check if enough money
+        and create the new one
+        """
+        buy_choice = input(f"{self.name} : Do you want to buy this turn (Y/N) ? (or enter if no) ")
+        if buy_choice == "y" or buy_choice == "Y":
+            line = input(f"{self.name}: Wich line would you place the new one (0-{self.game.nb_lines - 1}) ?")
+            print(f"List of character : \n"
+                  f"F - Fighter : {Character.__str__(Fighter)}\n"
+                  f"T - Tank : {Character.__str__(Tank)}\n"
+                  f"D - Duck : {Character.__str__(Duck)}")
+            char_choice = input(f"{self.name} : Wich Character do you want to buy ? ")
+            if line != "":
+                line = int(line)
+                if 0 <= line <= self.game.nb_lines - 1:
+                    if self.money >= Character.base_price:
+                        column = 0 if self.direction == +1 else self.game.nb_columns - 1
+                        if char_choice == "F" or char_choice == "f":
+                            Fighter(self, (line,column))
+                        elif char_choice == "T" or char_choice == "t":
+                            Tank(self, (line, column))
+                        elif char_choice == "D" or char_choice == "d":
+                            Duck(self, (line,column))
+
+
+class Fighter(Character):
+    base_price = 2
+    base_life = 7
+    base_strength = 3
+
+    def __str__(self):
+        return f"{self.name[:1]} - {self.name} : {self.base_price}$ - Force : {self.base_strength} - Life : {self.base_life}"
+
+    @property
+    def design(self):
+            return '@' if self.direction == 1 else '@'
+
+class Tank(Character):
+    base_price = 5
+    base_life = 10
+    base_strength = 1
+
+    def __str__(self):
+        return f"{self.name[:1]} - {self.name} : {self.base_price}$ - Force : {self.base_strength} - Life : {self.base_life}"
+
+    @property
+    def design(self):
+        return 'O' if self.direction == 1 else 'O'
+
+class Duck(Character):
+    base_price = 8
+    base_life = 10
+    base_strength = 20
+
+    def __str__(self):
+        return f"{self.name[:1]} - {self.name} : {self.base_price}$ - Force : {self.base_strength} - Life : {self.base_life}"
+
+    @property
+    def design(self):
+        return '^' if self.direction == 1 else '^'
 
 if __name__ == "__main__":
     print("""
